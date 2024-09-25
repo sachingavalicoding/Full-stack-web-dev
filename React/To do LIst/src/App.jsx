637d594bd5412bcc2673ca375e58c5b3d22a960e
Sachin Gavali <sachingavali321@gmail.com>
2024-09-25 21:49:58 +0530

    to do list

---
+++ React/To do LIst/src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return(
+    <div className="w-full min-h-screen bg-slate-900 ">
+     <main className="max-w-96 mx-auto border-2 ">
+     <h2 className="text-white text-center text-4xl font-medium"> TO do list </h2>
+     </main>
+    </div>
+  )
+}
+
+export default App;
