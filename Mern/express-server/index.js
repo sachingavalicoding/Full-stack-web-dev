d70e4b5e1aa16578171ef577a38e258d87c4f100
Sachin Gavali <sachingavali321@gmail.com>
2024-09-26 23:18:20 +0530

    express server created

--- Mern/express-server/index.js
+++ Mern/express-server/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
-
+const router = require("./router/auth-router.js");
 const app = express();
+
+app.use("/api/auth" , router);
 
 app.get("/", (req, res) => {
   res.status(200).send(" Welcome to mern stack Project ");
